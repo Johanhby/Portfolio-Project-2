fb7c5660850e159de3f088b6c9f0a44be49d5a59
Johanhby <johanhby@gmail.com>
2023-01-30 17:31:11 +0000

    add OPTION array of objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 const optionButtons = document.querySelectorAll('[data-option]')
-
+const OPTION = [
+    {
+        name: 'rock'
+        emoji: '(../images/rock.jpg)'
+        beats: 'scissors', 'lizard'
+    },
+    {
+        name: 'paper'
+        emoji: '(../images/paper.jpg)'
+        beats: 'rock', 'spock'
+    },
+    {
+        name: 'scissors'
+        emoji: '(../images/scissors.jpg)'
+        beats: 'paper', 'lizard'
+    },
+    {
+        name: 'lizard'
+        emoji: '(../images/lizard.jpg)'
+        beats: 'paper', 'spock'
+    },
+    {
+        name: 'spock'
+        emoji: '(../images/spock.jpg)'
+        beats: 'scissors', 'rock'
+    }
+]
 optionButtons.forEach(optionButtons => {
     optionButtons.addEventListener('click', e => {
         const optionName = optionButtons.dataset.option
