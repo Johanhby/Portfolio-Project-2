bb953eeec16188125e963dcf795f697af6876aab
Johanhby <johanhby@gmail.com>
2023-01-31 09:33:16 +0000

    add eventlisteners for option buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ const playerScore_div = document.getElementById("player-score");
 const computerScore_div = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
+const rock_button = document.querySelector("#ro");
+const paper_button = document.querySelector("#pa");
+const scissors_button = document.querySelector("#sc");
+const lizard_button = document.querySelector("#li");
+const spock_button = document.querySelector("#sp");
+
+rock_button.addEventListener('click', function() {
+    console.log("u click rock");
+})
